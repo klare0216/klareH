ea24bd6efebdf34b0db261f1deaf157fad0f1069
klareH <klare0216@gmail.com>
2018-01-13 00:02:22 +0800

    update: css animation

--- controller/main-control.js
+++ controller/main-control.js
@@ -21,15 +21,15 @@ app_main.controller('list', function($scope){
     {name:'Projects', enable:'f'},
     {name:'Works', enable:'t'},
     {name:'Blogs', enable:'f'},
-    {name:'News', enable:'t'}
+    {name:'Update Log', enable:'t'}
   ];
 });
 
 app_main.config(function($routeProvider) {
-  $routeProvider.when("/",{ templateUrl : "news.html"})
+  $routeProvider.when("/",{ templateUrl : "works.html"})
                 .when("/About KlareH",{ templateUrl : "about_kh.html"})
                 .when("/Projects",{ templateUrl : "projects.html"})
                 .when("/Works",{ templateUrl : "works.html"})
                 .when("/Blogs",{ templateUrl : "blogs.html"})
-                .when("/News",{ templateUrl : "news.html"});
+                .when("/Update Log",{ templateUrl : "news.html"});
 });
